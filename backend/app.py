762b9577c8b86654a32c8644e202fd34915b3b82
M.Akhtar Rayyan S <akhtarrayyan337@gmail.com>
2026-05-22 11:02:47 +0700

    fix: update CORS origin and clean structure

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 # --- Setup Flask
 app = Flask(__name__, static_url_path='', static_folder='script')
 
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "https://rayna-homemade.netlify.app"}})
 
 # --- Path ke database (fix di folder 'database')
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
